6f62bb87fa5fe9f51cf4f571baf16c7bbee43852
matheusfelipetp <matheusfelipetp@outlook.com>
2022-09-12 09:45:41 -0300

    fix: accordion.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSmooth from './modules/scroll-suave.js';
 import initAnimateScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
@@ -13,8 +13,10 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 const scrollSmooth = new ScrollSmooth('[data-menu="smooth"] a[href^="#"]');
 scrollSmooth.init();
 
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
+
 initAnimateScroll();
-initAccordion();
 initTabNav();
 initModal();
 initToolTip();
